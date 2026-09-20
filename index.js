43d53cbf159b5c357774535179207f48c3fc223d
Kushal Shrestha <shresthakushal097@gmail.com>
2026-09-20 21:41:20 +0545

    Its Web learning time

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const fs= require("fs");
 
 const myServer = http.createServer( (req, res)=>{
     // console.log(req.headers);   // if we only do console.log(req) then the req here work as a object that stores info of request 
-    const log = `${Date.now()}:New Request has been Received\n`;
+    const log = `${Date.now()}: ${req.url} New Request has been Received\n`;
     fs.appendFile("log.txt", log , (err, data)=> {
-        res.end("Thanks for your support");
+        switch(req.url){
+            case'/':res.end("Thanks for your support at Homepage");
+            break
+            case'/about': res.end("Hey there, I am Kush");
+            break
+            default: res.end("Sorry,NOT Found ");
+        }
     });
 } );
 
